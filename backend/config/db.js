9be10f93a6ed212641d26cd9fe7c19abafe9f42a
Leandro <leanfiadone@gmail.com>
2024-07-30 10:08:02 -0300

    mongo DB

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,9 @@ const mongoose = require("mongoose")
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
+
+
+      
       // Removed useFindAndModify
     })
 
