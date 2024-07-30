8851221d2bb1e39095a48d5e2b19d2d224bb5dcf
Leandro <leanfiadone@gmail.com>
2024-07-30 16:29:44 -0300

    estilos y reducer de detail

--- frontend/src/store/slices/storiesSlice.ts
+++ frontend/src/store/slices/storiesSlice.ts
@@ -3,26 +3,40 @@ import axios from "axios"
 
 // types/Story.ts
 export interface Story {
-  _id: string;
-  status: string;
-  title: string;
-  body: string;
-  user: string;
-  createdAt?: string; // Asegúrate de que esto esté presente
+  _id: string
+  status: string
+  title: string
+  body: string
+  user: string
+  createdAt?: string // Asegúrate de que esto esté presente
 }
-
 
 interface StoriesState {
   stories: Story[]
+  story: Story | null // Añadir esta línea
   loading: boolean
   error: string | null
 }
 
 const initialState: StoriesState = {
   stories: [],
+  story: null, // Añadir esta línea
   loading: false,
   error: null
 }
+
+
+
+// Asynchronous thunk action for fetching a specific story
+export const fetchStoryById = createAsyncThunk(
+  "stories/fetchStoryById",
+  async (id: string) => {
+    const response = await axios.get(`http://localhost:8080/stories/${id}`, {
+      withCredentials: true
+    })
+    return response.data.story
+  }
+)
 
 // Asynchronous thunk action for fetching stories
 export const fetchStories = createAsyncThunk(
@@ -57,6 +71,7 @@ export const deleteStory = createAsyncThunk(
   }
 )
 
+
 const storiesSlice = createSlice({
   name: "stories",
   initialState,
@@ -75,18 +90,31 @@ const storiesSlice = createSlice({
         state.loading = false
         state.error = action.error.message || "Failed to fetch stories"
       })
+      .addCase(fetchStoryById.pending, (state) => {
+        state.loading = true
+        state.error = null
+      })
+      .addCase(fetchStoryById.fulfilled, (state, action) => {
+        state.loading = false
+        state.story = action.payload
+      })
+      .addCase(fetchStoryById.rejected, (state, action) => {
+        state.loading = false
+        state.error = action.error.message || "Failed to fetch story"
+      })
       .addCase(deleteStory.pending, (state) => {
         state.loading = true
         state.error = null
       })
       .addCase(deleteStory.fulfilled, (state, action) => {
         state.loading = false
-        // Aquí podrías hacer algo si es necesario después de eliminar,
-        // por ejemplo, re-fetch historias si se necesita la actualización.
+        state.stories = state.stories.filter(
+          (story) => story._id !== action.payload
+        )
       })
       .addCase(deleteStory.rejected, (state, action) => {
         state.loading = false
-        state.error = action.payload as string || "Failed to delete story"
+        state.error = (action.payload as string) || "Failed to delete story"
       })
   }
 })
