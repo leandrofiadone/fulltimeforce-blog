3dc41012e2c8f8dc4dc2ffd762f4c38977089c35
Leandro <leanfiadone@gmail.com>
2024-08-04 19:58:03 -0300

    prueba

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -11,7 +11,7 @@ module.exports = function (passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: `${process.env.BACKEND_URL}/auth/google/callback`
+        callbackURL: `${process.env.BASE_URL}/auth/google/callback`
       },
       async (accessToken, refreshToken, profile, done) => {
         const newUser = {
@@ -44,7 +44,7 @@ module.exports = function (passport) {
       {
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: `${process.env.BACKEND_URL}/auth/github/callback`
+        callbackURL: `${process.env.BASE_URL}/auth/github/callback`
       },
       async (accessToken, refreshToken, profile, done) => {
         const newUser = {
